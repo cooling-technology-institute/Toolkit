5ab4ace835426d24ecd3a9305b9a03f5f8e015e5
renee <renee_geoffrion@hotmail.com>
2021-06-08 22:12:01 -0500

    Add setup

--- CTIToolkit/Properties/Settings.Designer.cs
+++ CTIToolkit/Properties/Settings.Designer.cs
@@ -34,5 +34,17 @@ namespace CTIToolkit.Properties {
                 this["UnitsSelection"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool Demo {
+            get {
+                return ((bool)(this["Demo"]));
+            }
+            set {
+                this["Demo"] = value;
+            }
+        }
     }
 }
