66d393806dbb544c6d7d06ac9f7cf01b72d4e3f6
renee <renee_geoffrion@hotmail.com>
2025-08-08 15:17:43 -0500

    Update version

--- CTIToolkit/Properties/AssemblyInfo.cs
+++ CTIToolkit/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Cooling Technology Institute")]
 [assembly: AssemblyProduct("Toolkit")]
-[assembly: AssemblyCopyright("Copyright ©  2018 - 2022")]
+[assembly: AssemblyCopyright("Copyright ©  2018 - 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.2.*")]
-[assembly: AssemblyFileVersion("4.2.0.0")]
+[assembly: AssemblyVersion("4.3.*")]
+[assembly: AssemblyFileVersion("4.3.0.0")]
